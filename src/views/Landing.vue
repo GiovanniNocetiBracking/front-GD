843e7d9309ebc79b25c61dbcaaa23f019e40dd7c
Giovanni Noceti <giovanni.noceti@gmail.com>
2021-02-26 21:32:12 -0300

    cambios responsive landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -441,10 +441,9 @@
           </div>
         </div>
         </section>
-        <div class="relative block py-24 lg:pt-0 bg-gray-900 mt-5"></div>
-        <section class="relative block py-24 lg:pt-0 bg-gray-900 mt-20" >
+        <section class="relative block py-24 lg:pt-0 bg-gray-900" >
           <div class="container mx-auto px-4">
-          <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
+          <div class="flex flex-wrap justify-center pt-8">
             <div class="w-full lg:w-6/12 px-4">
               <div
                 class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300"
